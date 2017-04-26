8dad667cf556a4ff7854a09fbc5454862f0bb7d1
Javier López Achondo <jilopez8@uc.cl>
2017-04-26 10:42:19 -0300

    scaffold modelos base

--- noflix/config/routes.rb
+++ noflix/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :articles
+  resources :subtitles
+  resources :reviews
+  resources :directors
+  resources :actors
+  resources :episodes
+  resources :seasons
+  resources :shows
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
---
+++ noflix/db/migrate/20170426133322_create_subtitles.rb
@@ -0,0 +1,8 @@
+class CreateSubtitles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :subtitles do |t|
+
+      t.timestamps
+    end
+  end
+end
