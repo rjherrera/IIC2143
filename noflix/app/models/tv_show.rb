28403c471d7f8cc9535dcb86973e6136b78441b2
Raimundo Herrera <rjherrera@uc.cl>
2017-05-29 10:39:28 -0400

    images showing correctly

--- noflix/app/models/tv_show.rb
+++ noflix/app/models/tv_show.rb
@@ -16,4 +16,12 @@ class TvShow < ApplicationRecord
     validates :start_year, presence: true
     validates :plot, presence: true
     validates :director_id, presence: true
+
+    def image_url
+        if self.id < 6
+            url = 'tv_shows_covers/' + self.id.to_s + '.jpg'
+        else
+            url = 'tv_shows_covers/' + (self.id % 5 + 1).to_s + '.jpg'
+        end
+    end
 end
