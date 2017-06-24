f7e42b5f7cfab64d09612f66fe436cd37c2dadb6
Sam <shgoldfarb@uc.cl>
2017-06-24 00:01:05 -0400

    removed code was necesary

--- noflix/app/models/tv_show.rb
+++ noflix/app/models/tv_show.rb
@@ -18,11 +18,10 @@ class TvShow < ApplicationRecord
     validates :director_id, presence: true
 
     def image_url
-        # if self.id < 6
-        #     url = 'tv_shows_covers/' + self.id.to_s + '.jpg'
-        # else
-        #     url = 'tv_shows_covers/' + (self.id % 5 + 1).to_s + '.jpg'
-        # end
-        url = 'tv_shows_covers/' + (self.id % 5 + 1).to_s + '.jpg'
+        if self.id < 6
+            url = 'tv_shows_covers/' + self.id.to_s + '.jpg'
+        else
+            url = 'tv_shows_covers/' + (self.id % 5 + 1).to_s + '.jpg'
+        end
     end
 end
