e7f79ddf2c8c61ebbcd7dfa9fc57d2b35c3f4af8
Raimundo Herrera <rjherrera@uc.cl>
2017-06-25 20:57:02 -0400

    implemented sendgrid mailing system

--- noflix/app/models/user.rb
+++ noflix/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :kids, class_name: "User", foreign_key: "father_id"
   belongs_to :father, class_name: "User", optional: true
   has_many :private_tv_shows, class_name: "TvShow"
--- noflix/db/schema.rb
+++ noflix/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624040436) do
+ActiveRecord::Schema.define(version: 20170625235538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,10 @@ ActiveRecord::Schema.define(version: 20170624040436) do
     t.boolean  "is_admin",               default: false
     t.integer  "father_id"
     t.string   "favourite_categories",   default: [],                 array: true
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["father_id"], name: "index_users_on_father_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
